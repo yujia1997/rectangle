d43bfd7b1978b5accbab6c913e7039368fd070c2
wangding <408542507@qq.com>
2019-03-16 09:47:33 +0800

    rectangle v0.3

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,27 @@
-/* global module: true */
-module.exports = function (grunt) {
-  grunt.initConfig({
-    eslint: {
-      options: {
-                configFile: '.eslintrc.json'
-          
-      },
-        target: ['*.js']
-        
-    },
-  csslint: {
-    options: {
-              csslintrc: '.csslintrc'
-          
-    },
-        src: '*.css'
-        
-  },
-  htmlhint: {
-    options: {
-              htmlhintrc: '.htmlhintrc'
-          
-    },
-          src: '*.html'
-                
-  }
-    
+ module.exports = function (grunt) {
+          htmlhintrc: '.htmlhintrc'
+                  
+},
+        src: '*.html'
+            },
+            mocha: {
+              test: {
+                  src: ['test/index.html'],
+                              
+              },
+              options: {
+                        run: true,
+                        reporter: 'Spec'
+                                        
+              }
+                  
+            }
   });
 
-    grunt.loadNpmTasks('grunt-contrib-csslint');
-      grunt.loadNpmTasks('grunt-htmlhint');
-        grunt.loadNpmTasks('grunt-eslint');
-
-          grunt.registerTask('lint', ['htmlhint', 'csslint', 'eslint']);
-
-};
+  grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-htmlhint');
+  grunt.loadNpmTasks('grunt-eslint');
+  grunt.loadNpmTasks('grunt-mocha');
+  grunt.registerTask('lint', ['htmlhint', 'csslint', 'eslint']);
+  grunt.registerTask('unitTest', ['mocha']);
+  };
