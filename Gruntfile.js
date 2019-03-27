693dcbc003d784dc60b2ddd5b6ec101ac5ef4da0
= <=>
2019-03-27 16:47:35 +0800

    v0.5

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@
           htmlhintrc: '.htmlhintrc'
                   
 },
-          src: 'index.html',
+          src: 'dist/index.html',
             },
             mocha: {
               test: {
@@ -37,13 +37,45 @@
             uglify: {
               release:{
                 files: {
-                            'dist/rectangle.js': 'rectangle.js',
-                                      'dist/calc.js': 'calc.js'
+                      'dist/bundle.min.js': 'dist/bundle.js',
                                                 
                 }
                              
               }
-        }
+             
+        },
+        useminPrepare: {
+                html: 'index.html',
+                options: {
+                          dest: 'dist'
+                                  
+                }
+                    
+        },
+        usemin: {
+                html: ['dist/index.html']
+                      
+        },
+        concat: {
+          options: {
+                    separator: ';'
+                            
+          },
+          js: {
+                    src: ['rectangle.js', 'calc.js'],
+                            dest: 'dist/bundle.js'
+                                    
+          }
+              
+        },
+            clean: ['dist/bundle.js', '.tmp'],
+            copy: {
+              html: {
+                        src: './index.html',
+                                dest: './dist/index.html'
+                                        
+              }
+            }
   });
 
   grunt.loadNpmTasks('grunt-contrib-csslint');
@@ -53,7 +85,11 @@
   grunt.loadNpmTasks('grunt-contrib-htmlmin');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-usemin');
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.registerTask('lint', ['htmlhint', 'csslint', 'eslint']);
   grunt.registerTask('unitTest', ['mocha']);
-  grunt.registerTask('minify', ['htmlmin', 'cssmin', 'uglify']);
+  grunt.registerTask('release', ['copy', 'useminPrepare', 'concat', 'uglify', 'usemin', 'cssmin', 'htmlmin', 'clean']);
   };
