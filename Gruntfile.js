c2c7084fdd3a8f0ed7b6dbde50355b47c222edd6
= <=>
2019-03-27 16:20:16 +0800

    v0.4

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@
           htmlhintrc: '.htmlhintrc'
                   
 },
-        src: '*.html'
+          src: 'index.html',
             },
             mocha: {
               test: {
@@ -14,14 +14,46 @@
                         reporter: 'Spec'
                                         
               }
+
                   
-            }
+            },
+            htmlmin: {
+              options: {
+                        collapseWhitespace: true,
+                                preserveLineBreaks: false
+                                        
+              },
+              files: {
+                        src: './index.html',
+                                dest: 'dist/index.html'
+                                        
+              }
+                  
+            },
+            cssmin: {
+                    'dist/rectangle.css': 'rectangle.css'
+                          
+            },
+            uglify: {
+              release:{
+                files: {
+                            'dist/rectangle.js': 'rectangle.js',
+                                      'dist/calc.js': 'calc.js'
+                                                
+                }
+                             
+              }
+        }
   });
 
   grunt.loadNpmTasks('grunt-contrib-csslint');
   grunt.loadNpmTasks('grunt-htmlhint');
   grunt.loadNpmTasks('grunt-eslint');
   grunt.loadNpmTasks('grunt-mocha');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.registerTask('lint', ['htmlhint', 'csslint', 'eslint']);
   grunt.registerTask('unitTest', ['mocha']);
+  grunt.registerTask('minify', ['htmlmin', 'cssmin', 'uglify']);
   };
